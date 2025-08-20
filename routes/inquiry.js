27bd4cb3d87fde66fa16646bb2d6bf92c616cb25
mac <habeebmustapha96@gmail.com>
2025-08-20 09:20:03 +0100

    inquiry-fix

--- routes/inquiry.js
+++ routes/inquiry.js
@@ -8,15 +8,16 @@ import {
   addCommunicationLog,
   deleteInquiry,
 } from "../controllers/inquiry.js";
+import { verifyAdmin, verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/", createInquiry);
-router.get("/", getInquiries);
-router.get("/:id", getInquiryById);
-router.post("/:id/messages", addMessageToInquiry);
-router.put("/:id/status", updateInquiryStatus);
-router.post("/:id/communications", addCommunicationLog);
-router.delete("/:id", deleteInquiry);
+router.post("/", verifyToken, createInquiry);
+router.get("/", verifyToken, getInquiries);
+router.get("/:id", verifyToken, getInquiryById);
+router.post("/:id/messages", verifyToken, addMessageToInquiry);
+router.put("/:id/status", verifyAdmin, updateInquiryStatus);
+router.post("/:id/communications", verifyToken, addCommunicationLog);
+router.delete("/:id", verifyAdmin, deleteInquiry);
 
 export default router;
